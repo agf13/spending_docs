5fc377025f7220e4b353ba7100f66608bd8920b4
agf13 <sebastianpomirleanu@yahoo.com>
2026-07-29 10:27:31 +0300

    Renamed variables and deleted some prints in ReceiptScanWidget

--- lib/widgets/receipt_scan_widget.dart
+++ lib/widgets/receipt_scan_widget.dart
@@ -7,6 +7,8 @@ import 'package:mime/mime.dart';
 import 'package:spending_docs/blocs/receipt_items_list_cubit.dart';
 import 'package:spending_docs/blocs/receipts_list_cubit.dart';
 import 'package:spending_docs/core/receipt_analysis/receipt_analysis_service.dart';
+import 'package:spending_docs/database/app_database.dart'
+    show Receipt, ReceiptItem;
 import 'package:spending_docs/repositories/receipt_items_repository.dart';
 import 'package:spending_docs/repositories/receipts_repository.dart';
 
@@ -18,14 +20,26 @@ class ReceiptScanWidget extends StatefulWidget {
 }
 
 class _ReceiptScanWidgetState extends State<ReceiptScanWidget> {
-  XFile? pickedFile;
+  XFile? _pickedFile;
+
+  Receipt? receipt;
+  List<ReceiptItem> itemList = [];
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: .center,
+      crossAxisAlignment: .center,
+      mainAxisSize: .min,
       children: [
         // Scan button
         scanButton(context),
+
+        // Spacer
+        SizedBox(height: 10),
+
+        // Preview of scanned receipt
+        previewReceipt(context),
       ],
     );
   }
@@ -39,20 +53,77 @@ class _ReceiptScanWidgetState extends State<ReceiptScanWidget> {
     );
   }
 
+  Widget previewReceipt(BuildContext context) {
+    if (receipt != null) {
+      return SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: .start,
+          crossAxisAlignment: .start,
+          mainAxisSize: .min,
+          children: [
+            // Receipt
+            ...receiptAsText(context),
+
+            // Separator
+            ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: 200),
+              child: Divider(color: Colors.red)),
+
+            // Receipt items
+            ...receiptItemsAsText(context),
+          ],
+        ),
+      );
+    }
+
+    return SizedBox.shrink();
+  }
+
+  List<Widget> receiptAsText(BuildContext context) {
+    List<Widget> widgets = [];
+    if (receipt != null) {
+      widgets.add(oneRowLine('Amount:', receipt!.amount.toString()));
+      widgets.add(oneRowLine('StoreName:', receipt!.storeName ?? ''));
+      widgets.add(oneRowLine('Date', receipt!.date.toString()));
+      widgets.add(oneRowLine('Card', receipt!.card ?? ''));
+    }
+
+    return widgets;
+  }
+
+  List<Widget> receiptItemsAsText(context) {
+    List<Widget> widgets = [];
+    for (var item in itemList) {
+      widgets.add(oneRowLine('ItemName:', item.itemName));
+      widgets.add(oneRowLine('Price:', item.price.toString()));
+    }
+
+    return widgets;
+  }
+
+  Widget oneRowLine(String label, String value) {
+    return SingleChildScrollView(
+      child: Row(
+        mainAxisAlignment: .start,
+        crossAxisAlignment: .start,
+        mainAxisSize: .min,
+        children: [Text(label), SizedBox(width: 10, height: 10), Text(value)],
+      ),
+    );
+  }
+
   Future<void> handleScan(BuildContext context) async {
     await pickPhoto();
     Map<String, dynamic>? json = await analyzeImage();
 
     if (json != null) {
-      print('Extracting items');
-      extractAndInsertElements(json, context);
-
-      // Refresh the list of items
-      await context.read<ReceiptsListCubit>().getItems();
-      await context.read<ReceiptItemsListCubit>().getItems();
-    } else {
-      print('Json was null');
+      await insertItems(json, context);
     }
+  }
+
+  Future<void> refreshLists(BuildContext context) async {
+    await context.read<ReceiptsListCubit>().getItems();
+    await context.read<ReceiptItemsListCubit>().getItems();
   }
 
   Future<void> pickPhoto() async {
@@ -70,14 +141,14 @@ class _ReceiptScanWidgetState extends State<ReceiptScanWidget> {
     if (file == null) return;
     print('Receipt loaded');
 
-    pickedFile = file;
+    _pickedFile = file;
   }
 
   Future<Map<String, dynamic>?> analyzeImage() async {
     print('Starting analysis');
-    if (pickedFile == null) return null;
-    final bytes = await _xFileToUint8List(pickedFile!);
-    final mimeType = await mimeTypeFromXFile(pickedFile!);
+    if (_pickedFile == null) return null;
+    final bytes = await _xFileToUint8List(_pickedFile!);
+    final mimeType = await mimeTypeFromXFile(_pickedFile!);
 
     if (mimeType == null) return null;
 
@@ -87,36 +158,39 @@ class _ReceiptScanWidgetState extends State<ReceiptScanWidget> {
     return await analysisService.analyzeReceiptImage(bytes, mimeType);
   }
 
-  Future<void> extractAndInsertElements(
+  Future<void> insertItems(
     Map<String, dynamic> json,
     BuildContext context,
   ) async {
     // Add receipt
-    print('Extract receipt');
     final receiptCompanion = ReceiptAnalysizService.extractReceipt(json);
-
-    // Return if the receiptCompanion cannot be extracted
     if (receiptCompanion == null) return;
 
-    // Insert the receipt and store the id
-    print('Insert receipt');
     final receiptId = await context.read<ReceiptsRepository>().add(
       receiptCompanion,
     );
 
     // Extract the list of items and prepare them with the receiptId as parent receipt id
-    print('Extract items from receipt');
-    final itemList = ReceiptAnalysizService.extractReceiptItems(
+    final itemListCompanion = ReceiptAnalysizService.extractReceiptItems(
       json,
       receiptId,
     );
 
-    // Add all elements to the database
-    print('Insert all items from receipt');
     final receiptItemsRepository = context.read<ReceiptItemsRepository>();
-    for (final item in itemList) {
+    for (final item in itemListCompanion) {
       receiptItemsRepository.add(item);
     }
+
+    // Prepare preview items
+    receipt = await context.read<ReceiptsRepository>().getById(receiptId);
+    itemList = await context.read<ReceiptItemsRepository>().getByReceiptId(
+      receipt!.id,
+    );
+
+    setState(() {
+      receipt = receipt;
+      itemList = itemList;
+    });
   }
 
   Future<Uint8List> _xFileToUint8List(XFile image) async {
